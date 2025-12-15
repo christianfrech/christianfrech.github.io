36e9d489989065b6dbf9d4eb4d29b6899324796d
christianfrech <64506602+christianfrech@users.noreply.github.com>
2025-12-15 20:09:05 +0000

    Deploying to gh-pages from @ christianfrech/christianfrech.github.io@c4ca47ca85dc13e3077bded46bea37f0c1118f92 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,21 +501,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/christianfrech_resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%63%68%72%69%73%74%69%61%6E%66%72%65%63%68%30%30@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -536,7 +529,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("", "_blank");
         },
       },{
       id: 'light-theme',
